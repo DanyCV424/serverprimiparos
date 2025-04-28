da6c50e9eba5ca7566c627be9cf65cdef1fa082c
DnovoaB <danielfnovoab@gmail.com>
2025-04-28 15:30:15 -0500

    Primer commit a github

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res)=>{
 
 //const/let nombre = (()=>{ codigo });
 
-const PORT = 127.0.0.1:5500/paginagustos/indexedDB.html;
+const PORT = 5500
 
 server.listen(PORT, () => {
     console.log(`Servidor escuchando el puerto ${PORT}`);
